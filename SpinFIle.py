dfe00b6b8e87903400cac0179052e762ebea0809
sangdo lee <mydaniel9@gmail.com>
2024-10-24 06:04:54 +0900

    change the link to the github repo

--- SpinFIle.py
+++ SpinFIle.py
@@ -1,4 +1,4 @@
-#A4, Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
+#A4, Sangdo Lee https://github.com/SSangdodo/A4-Word-Spinner
 from Spinner import *
 import string
 
@@ -19,7 +19,7 @@ def main():
     words = make_list_from_file("essay.txt")
     spinned = Spinner(words)
     #make a class using modified words
-    print("Original : " spinned.return_original_str())
+    print("Original : " +spinned.return_original_str())
     #from the words of the original file, make them into a one full string
     for option_counter in range(1,4):
         #for 1~3
--- Spinner.py
+++ Spinner.py
@@ -1,4 +1,4 @@
-#A4 Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
+#A4 Sangdo Lee https://github.com/SSangdodo/A4-Word-Spinner
 from random import *
 def make_list_from_file(filename):
     file_content = open(filename,"r")
