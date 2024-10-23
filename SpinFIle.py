5b2f19703909c3a21a0f99c443256d53dbabc5b7
sangdo lee <mydaniel9@gmail.com>
2024-10-24 04:06:01 +0900

    updated the :SpinFIle.py with the function to read the given file and make list with the words converted for utilization

--- SpinFIle.py
+++ SpinFIle.py
@@ -1,1 +1,9 @@
-
+#A4, Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
+from Spinner import *
+import string
+def make_list_from_file(filename):
+    file_content = open(filename,"r")
+        returning_list = [words for words in lines.lower().translate(str.maketrans('', '', string.punctuation)).lower().split(): ]
+        #make the list with the lowered and puncuation removed words
+    file_content.close()
+    return returning_list
