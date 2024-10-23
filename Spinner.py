5a391d4a22c59c85836081265b8c2aa352022da3
sangdo lee <mydaniel9@gmail.com>
2024-10-24 04:35:39 +0900

    created the function to make a dictionary by a given list.

--- Spinner.py
+++ Spinner.py
@@ -1,5 +1,6 @@
 #A4 Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
 from random import *
+
 def make_list_from_file(filename):
     file_content = open(filename,"r")
     
@@ -7,3 +8,14 @@ def make_list_from_file(filename):
     #make the list from the lines in file content, removing the lines with the tab and spaces
     
     return returning_list
+
+def make_response_dictionary(response_list):
+    keyword_and_response = {}
+
+    for items in response_list:
+        keyword,synonyms = items.split(":")
+        #split the keyword section and synonym section
+        keyword_and_response[keyword] = synonyms.split(",")
+        #store the key and synonyms into a dictonary
+    
+        return keyword_and_response
