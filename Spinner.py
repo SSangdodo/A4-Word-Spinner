908224ce04fcae3627c64d98022dfcdd69aabadc
sangdo lee <mydaniel9@gmail.com>
2024-10-24 04:31:13 +0900

    made the function that it reads the given file and strips the tab and the spaces, returning a line with only the words

--- Spinner.py
+++ Spinner.py
@@ -2,6 +2,8 @@
 from random import *
 def make_list_from_file(filename):
     file_content = open(filename,"r")
-    returning_list = [lines.strip("\n").strip(" ") for lines in file_content]
+    
+    returning_list = [lines for lines in file_content.strip("\n").strip(" ")]
     #make the list from the lines in file content, removing the lines with the tab and spaces
+    
     return returning_list
