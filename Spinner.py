b3c81b3f710ebad62678b52197e3e68aeeead8df
sangdo lee <mydaniel9@gmail.com>
2024-10-24 04:39:55 +0900

    made the class definition, making it so that the class reads the list given from the SPinFile.py and the synonyms.txt. converting the words with 50% probability and changing if needed

--- Spinner.py
+++ Spinner.py
@@ -19,3 +19,21 @@ def make_response_dictionary(response_list):
         #store the key and synonyms into a dictonary
     
         return keyword_and_response
+
+class Spinner():
+    def __init__(self,file_words):
+            self.file_words_list = file_words
+            self.synonyms_data = make_response_dictionary(make_list_from_file("synonyms-simplified.txt"))
+    
+        def convert_synonyms(self):
+            returning_list = []
+            for words in self.file_words_list:
+                if self.synonyms_data.get(str(words),1) != 1 and randint(1,2) == 2:
+                # if the word exit within synonym dictionary and if it meets the 50% chance of changing the words
+                returning_list.append(self.synonyms_data[str(words)][randint(0,len(self.synonyms_data[words])-1)])
+                #append to the returning list of random synonyms from the specified key
+            else:
+                returning_list.append(words)
+                #no match of synonym or 50% of unchanged words
+
+        return " ".join(self.file_words_list) , " ".join(returning_list)
