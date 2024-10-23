6293a148eb0f8ab6964d577df6e23bbcc7f0bf2c
sangdo lee <mydaniel9@gmail.com>
2024-10-24 04:09:39 +0900

    fixed he errors with the SpinFIle

--- SpinFIle.py
+++ SpinFIle.py
@@ -3,6 +3,7 @@ from Spinner import *
 import string
 def make_list_from_file(filename):
     file_content = open(filename,"r")
+    for lines in files:    
         returning_list = [words for words in lines.lower().translate(str.maketrans('', '', string.punctuation)).lower().split(): ]
         #make the list with the lowered and puncuation removed words
     file_content.close()
