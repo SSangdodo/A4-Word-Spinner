d14eaa25ff605cf7d13cd46e2eedda462f85e553
sangdo lee <mydaniel9@gmail.com>
2024-10-24 07:20:37 +0900

    specified some of the explanation to the code and removed repetitive aspect from the code

--- Spinner.py
+++ Spinner.py
@@ -5,7 +5,7 @@ def make_list_from_file(filename):
     returning_list = []
     for lines in file_content:
         returning_list.append(lines.strip("\n").strip(" "))
-        #extract space and tabs from the lines
+        #extract space and tabs from the lines and add in a list
     file_content.close()
     return returning_list
 
@@ -23,15 +23,17 @@ class Spinner():
     def __init__(self,file_words):
             self.file_words_list = file_words
             self.synonyms_data = make_response_dictionary(make_list_from_file("synonyms-simplified.txt"))
+    
     def return_original_str(self):
         return " ".join(self.file_words_list)
+    
     def convert_synonyms(self):
         returning_list = []
         for words in self.file_words_list:
             if self.synonyms_data.get(str(words),1) != 1 and randint(1,2) == 2:
                 # if the word exit within synonym dictionary and if it meets the 50% chance of changing the words
-                returning_list.append(self.synonyms_data[str(words)][randint(0,len(self.synonyms_data[words])-1)])
-                #append to the returning list of random synonyms from the specified key
+                returning_list.append(self.synonyms_data[words][randint(0,len(self.synonyms_data[words])-1)])
+                #append to the returning list, from random synonyms of the specified key
             else:
                 returning_list.append(words)
                 #no match of synonym or 50% of unchanged words
